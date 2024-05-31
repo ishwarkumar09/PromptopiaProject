7a596b0835e0d698a8677d2d4a35e41cdd85293e
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-05-31 17:17:31 +0530

    data base error debugging

--- utils/database.js
+++ utils/database.js
@@ -12,9 +12,7 @@ export const connectTODB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "share_prompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      dbName: "share_prompt"
     });
 
     isConnected = true;
